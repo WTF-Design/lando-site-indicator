552fc90fac9d07452c3e226966a8a111a2dd6178
Lasse Mattila <lasse.mattila@wtfdesign.fi>
2024-01-17 14:17:13 +0200

    Update script internal description

--- script.user.js
+++ script.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Lando site indicator
-// @description To easily visually distinguish dev sites from production etc
+// @description To easily visually distinguish Lando dev sites from production etc
 // @icon        data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 140.5 140.5'%3E%3Cpath fill='%23df4090' d='M70.3 0a70.3 70.3 0 1 0 70.2 70.3A70.3 70.3 0 0 0 70.2 0Zm0 9a61.4 61.4 0 0 1 57.4 39.8c-4.5-2.7-13.5-3.6-25-2.6-1.2 0-2.7 1 0 1.2 3.4.2 8.3 1.3 9.7 4.1 2.6 5.5-7.6 9-11 10.8a38 38 0 0 0-.4-1.3 32.1 32.1 0 1 0-61.6 18l.5 1.3c-3.9.4-14.4 3-15-3.1-.4-3.1 3.2-6.7 5.8-8.7 2.2-1.6.5-1.6-.6-1-10.4 5.4-17.5 11-19.8 15.8a61.3 61.3 0 0 1 60-74.5Zm0 122.6a61.3 61.3 0 0 1-57.7-40.3c5.6 3.5 18.6 4 35 1.5a32.1 32.1 0 0 0 54-15.9c15-6.7 25.5-14 28.5-20a61.3 61.3 0 0 1-59.9 74.7Z'/%3E%3C/svg%3E
 // @namespace   wtfdesign
 // @match       https://*.lndo.site/*
